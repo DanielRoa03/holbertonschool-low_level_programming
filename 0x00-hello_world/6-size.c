8fd8662d3b3fd3d2010641989b124ca89e18de2a
DanielRoa03 <roadaniel200@gmail.com>
2022-02-10 18:59:04 -0800

    bytes C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of int: %zu bytes\n", sizeof(char));
+	printf("Size of int: %zu bytes\n", sizeof(int));
+	printf("Size of int: %zu bytes\n", sizeof(long int));
+	printf("Size of int: %zu bytes\n", sizeof(long long int));
+	printf("Size of int: %zu bytes\n", sizeof(float));
+	return (0);
+
+}
