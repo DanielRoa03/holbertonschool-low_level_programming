82d9368954fd33f5d4e92d971ac963f34297dcca
DanielRoa03 <roadaniel200@gmail.com>
2022-02-11 16:33:06 -0800

    C

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the numbers from zero to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 48;
+
+	while (c < 58)
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
