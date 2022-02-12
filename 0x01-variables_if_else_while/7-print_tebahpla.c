d668ddbc4dca8d3c29fda8ebf3f6ca311519c880
DanielRoa03 <roadaniel200@gmail.com>
2022-02-11 16:37:20 -0800

    C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet backwards
+ * 
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 122;
+
+	while (c > 96)
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
