c52cac8d6c54ce6d4deadbb50dff03d2980fc474
DanielRoa03 <roadaniel200@gmail.com>
2022-02-11 16:22:22 -0800

    C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet but not the e and q
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c = 97;
+	while (c < 123)
+	{
+		if ((c != 101) && (c != 113))
+		{
+		putchar(c);
+		}
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
